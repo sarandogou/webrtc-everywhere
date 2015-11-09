3ddb02dfc77b4640af361886d30f4f58783655b8
Mamadou DIOP <sarandogou0@gmail.com>
2015-11-10 00:26:48 +0100

    Update version for windows (2.2.3)

--- ie/webrtceverywhere_i.c
+++ ie/webrtceverywhere_i.c
@@ -6,10 +6,10 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Sat Nov 07 03:18:03 2015
+/* at Mon Nov 09 23:55:41 2015
  */
 /* Compiler settings for webrtceverywhere.idl:
-    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
+    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.00.0603 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
