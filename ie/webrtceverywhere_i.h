0ec1f73efeca82120638197bdbf94d0384f1a7a6
Jeremy Mao <maoyujie@agora.io>
2016-01-20 14:43:38 +0800

    Fix build with webrtc branch 47

--- ie/webrtceverywhere_i.h
+++ ie/webrtceverywhere_i.h
@@ -4,10 +4,10 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Mon Nov 09 23:55:41 2015
+/* at Fri Jan 15 16:29:46 2016
  */
 /* Compiler settings for webrtceverywhere.idl:
-    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.00.0603 
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
